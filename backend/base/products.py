6a3629036729dd8f6bc9ba13abd22e7127d338fd
Denis <deniscapece@hotmail.com>
2022-01-18 20:27:17 +0100

    [backend] install + first test

---
+++ backend/base/products.py
@@ -0,0 +1,47 @@
+products = [
+    {
+        '_id': '1',
+        'name': 'Bracelet Upcyclé Lapis',
+        'image': '/images/bracelet1.webp',
+        'description': "Bracelet upcyclé créé et réalisé à la main en Belgique et composé exclusivement d'éléments en or 24K, pierres de Lapis Lasuli et d' Amazonite et médaille émail vintage.",
+        'category': 'Bracelet',
+        'collection': 'Upcycled',
+        'price': '75',
+    },
+    {
+        '_id': '2',
+        'name': 'Bracelet Upcyclé Ambre',
+        'image': '/images/bracelet2.webp',
+        'description': "Bracelet ucyclé créé et réalisé à la main en Belgique et composé exclusivement de pierre d'ambre, d'élements plaqué or 24K et de médaille en émail vintage.",
+        'category': 'Bracelet',
+        'collection': 'Upcycled',
+        'price': '75',
+    },
+    {
+        '_id': '3',
+        'name': "Boucles d'oreilles Corail",
+        'image': '/images/boucles1.webp',
+        'description': "Boucles d'oreilles perceuses créées et réalisés à la main en Belgique et composées exclusivement d'éléments en or plaqué 24K, corail recyclé certifié et pierre d'Amazonite.",
+        'category': "Boucles d'oreilles",
+        'collection': 'Lina',
+        'price': '65',
+    },
+    {
+        '_id': '4',
+        'name': "Créoles Or & Cristal vintage",
+        'image': '/images/boucles2.webp',
+        'description': "Créoles upcyclées composées exclusivement de pièces en or plaqué 18 carats et d'éléments de bijoux vintage en cristal récomposés. Réalisé à la main en Belgique. Durable et responsable.",
+        'category': "Boucles d'oreilles",
+        'collection': 'Marie-Louise',
+        'price': '65',
+    },
+    {
+        '_id': '5',
+        'name': "Créoles Or & Cristal vintage",
+        'image': '/images/collier1.webp',
+        'description': "Collier upcyclé réalisé à la main en Belgique et composé exclusivement d'éléments en or plaqué 24K, de corail certifié recyclé et d'une médaille religieuse vintage en email et metal doré.",
+        'category': "Collier",
+        'collection': 'Lina',
+        'price': '75',
+    }
+]
