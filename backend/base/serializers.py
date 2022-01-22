13e4814e8204e48e5bbbfdd42826216e0b29f409
Denis <deniscapece@hotmail.com>
2022-01-22 16:32:21 +0100

    [backend] -> token rest_framework_simplejwt

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -3,6 +3,20 @@ from django.contrib.auth.models import User
 from .models import *
 
 
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'name']
+
+    def get_name(self, obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+        return name
+
+
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
